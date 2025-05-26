caf03cf8cbc4aaf6ed8e85a1142e965d92910ac1
stanley <stanley.sunliang@gmail.com>
2025-05-26 12:51:46 +0800

    change upload code

--- upload_hugging_face.py
+++ upload_hugging_face.py
@@ -1,11 +1,49 @@
+# from huggingface_hub import login
+# from transformers import BertTokenizer, BertForSequenceClassification
+
+# login("withYourFaceHubToken")
+
+# model = BertForSequenceClassification.from_pretrained("./bert_sentiment_model")
+# tokenizer = BertTokenizer.from_pretrained("./bert_sentiment_model")
+
+
+# model.push_to_hub("bert_sentiment")
+# tokenizer.push_to_hub("bert_sentiment")
 from huggingface_hub import login
-from transformers import BertTokenizer, BertForSequenceClassification
-
-login("withYourFaceHubToken")
-
-model = BertForSequenceClassification.from_pretrained("./bert_sentiment_model")
-tokenizer = BertTokenizer.from_pretrained("./bert_sentiment_model")
+from transformers import BertForSequenceClassification, RobertaForSequenceClassification, DebertaV2ForSequenceClassification, BertTokenizer, RobertaTokenizer, AutoTokenizer
 
 
-model.push_to_hub("bert_sentiment")
-tokenizer.push_to_hub("bert_sentiment")
+login("")  
+
+models = [
+    ("bert_sentiment_5000", "./BERT/best_bert_model_5000"),
+    ("bert_sentiment_10000", "./BERT/best_bert_model_10000"),
+    ("bert_sentiment_30000", "./BERT/best_bert_model_30000"),
+    ("bert_sentiment_50000", "./BERT/best_bert_model_50000"),
+    ("roberta_sentiment_30000", "./RoBERT/best_roberta_model_10000"),
+    ("roberta_sentiment_30000", "./RoBERT/best_roberta_model_30000"),
+    ("roberta_sentiment_50000", "./RoBERT/best_roberta_model_50000"),
+    ("roberta_sentiment_100000", "./RoBERT/best_roberta_model_100000"),
+    ("deberta_sentiment_5000", "./DeBERT/best_deberta_model_5000"),
+    ("deberta_sentiment_50000", "./DeBERT/best_deberta_model_50000"),
+]
+
+for repo_name, model_path in models:
+    print(f"\nUploading {repo_name} from {model_path}...")
+
+    if "roberta" in repo_name:
+        model = RobertaForSequenceClassification.from_pretrained(model_path)
+        tokenizer = RobertaTokenizer.from_pretrained(model_path)
+    elif "deberta" in repo_name:
+        model = DebertaV2ForSequenceClassification.from_pretrained(model_path)
+        tokenizer = AutoTokenizer.from_pretrained(model_path)
+    else:
+        model = BertForSequenceClassification.from_pretrained(model_path)
+        tokenizer = BertTokenizer.from_pretrained(model_path)
+
+    model.push_to_hub(f"dfafdsaf/{repo_name}")
+    tokenizer.push_to_hub(f"dfafdsaf/{repo_name}")
+
+    print(f"{repo_name} uploaded successfully!")
+
+print("\nAll models uploaded successfully!")
